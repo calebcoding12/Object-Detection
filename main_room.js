093995ac9384a809945a8761bb4c956fc3d2806f
calebcoding12 <107775348+calebcoding12@users.noreply.github.com>
2022-12-13 21:12:08 -0600

    Add files via upload

---
+++ main_room.js
@@ -0,0 +1,30 @@
+img="";
+status1="";
+function preload(){
+    img = loadImage("Room.jpg");
+}
+function setup(){
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+function draw(){
+    image(img, 0,0, 640,420)
+}
+function modelLoaded(){
+    status1 = true;
+    console.log("Model is loaded");
+    objectDetector.detect(img, gotResults);
+}
+function gotResults(error, results){
+    if (error){
+        console.error(error);
+    }
+    else {
+        console.log(results);
+    }
+}
+function home(){
+    window.location = "index.html"
+}
